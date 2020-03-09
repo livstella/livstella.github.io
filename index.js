dc37d6dbb6241ebc36c987810c527178e28c4d19
Liv.R <livreinhold@gmail.com>
2020-03-09 16:59:29 +0100

    Group project

---
+++ index.js
@@ -0,0 +1,18 @@
+button1.addEventListener("click", function myFunction(){
+ alert("Share this link with your friends: https://runningonrealfood.com/vegan-buffalo-cauliflower-wraps/");
+});
+button2.addEventListener("click", function myFunction(){
+ alert("Share this link with your friends: https://runningonrealfood.com/brown-rice-burrito-bowl/");
+});
+button3.addEventListener("click", function myFunction(){
+ alert("Share this link with your friends: https://runningonrealfood.com/vegan-sweet-potato-tofu-bowls/");
+});
+button4.addEventListener("click", function myFunction(){
+ alert("Share this link with your friends: http://www.eatingwell.com/recipe/268220/vegan-mushroom-stroganoff/");
+});
+button5.addEventListener("click", function myFunction(){
+ alert("Share this link with your friends: http://www.eatingwell.com/recipe/257348/vegan-pancakes/");
+});
+button6.addEventListener("click", function myFunction(){
+ alert("Share this link with your friends: https://runningonrealfood.com/chickpea-veggie-burgers/");
+});
